604602b0e0ec9d5eac271bb231000b0e8664dad5
Radosław Loth <r.loth@icloud.com>
2021-10-10 22:01:56 +0200

    Bootstrap
    Added:
    bootstrap
    jquery
    sign in page component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { CoverComponent } from './cover/cover.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    CoverComponent,
   ],
   imports: [
     BrowserModule
