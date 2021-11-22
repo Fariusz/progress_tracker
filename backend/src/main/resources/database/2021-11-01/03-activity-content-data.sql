570a83f5993edb215fa0e1f9ea6cfcc62228aaac
Radosław Loth <r.loth@icloud.com>
2021-11-22 10:34:27 +0100

    Update 03-activity-content-data.sql
    Changed insert numeration.

--- backend/src/main/resources/database/2021-11-01/03-activity-content-data.sql
+++ backend/src/main/resources/database/2021-11-01/03-activity-content-data.sql
@@ -1,16 +1,14 @@
 --liquibase formatted sql
 --changeset rloth:3
-insert into activity (id, activity_name, created) values (100, 'activity', '2021-04-20T00:00');
-insert into activity (id, activity_name, created) values (101, 'activity1', '2020-04-20T13:14:05.724885500');
-insert into activity (id, activity_name, created) values (102, 'activity2', '2020-04-20T13:14:05.724885500');
-insert into activity (id, activity_name, created) values (103, 'activity3', '2020-04-20T13:14:05.724885500');
-insert into activity (id, activity_name, created) values (104, 'activity4', '2020-04-20T13:14:05.724885500');
-insert into activity (id, activity_name, created) values (105, 'activity5', '2020-04-20T13:14:05.724885500');
+insert into activity (id, activity_name, created) values (1, 'activity1', '2020-04-20T13:14:05.724885500');
+insert into activity (id, activity_name, created) values (2, 'activity2', '2020-04-20T13:14:05.724885500');
+insert into activity (id, activity_name, created) values (3, 'activity3', '2020-04-20T13:14:05.724885500');
+insert into activity (id, activity_name, created) values (4, 'activity4', '2020-04-20T13:14:05.724885500');
+insert into activity (id, activity_name, created) values (5, 'activity5', '2020-04-20T13:14:05.724885500');
 
 --changeset rloth:4
-insert into content (id, activity_id, content, created) values (100, 100,'content0', '2020-04-20T13:14:05.724885500');
-insert into content (id, activity_id, content, created) values (101, 101,'content1', '2020-04-20T13:14:05.724885500');
-insert into content (id, activity_id, content, created) values (102, 102, 'content2', '2020-04-20T13:14:05.724885500');
-insert into content (id, activity_id, content, created) values (103, 103, 'content3', '2020-04-20T13:14:05.724885500');
-insert into content (id, activity_id, content, created) values (104, 104, 'content4', '2020-04-20T13:14:05.724885500');
-insert into content (id, activity_id, content, created) values (105, 105, 'content5', '2020-04-20T13:14:05.724885500');
+insert into content (id, activity_id, content, created) values (1, 1,'content1', '2020-04-20T13:14:05.724885500');
+insert into content (id, activity_id, content, created) values (2, 2, 'content2', '2020-04-20T13:14:05.724885500');
+insert into content (id, activity_id, content, created) values (3, 3, 'content3', '2020-04-20T13:14:05.724885500');
+insert into content (id, activity_id, content, created) values (4, 4, 'content4', '2020-04-20T13:14:05.724885500');
+insert into content (id, activity_id, content, created) values (5, 5, 'content5', '2020-04-20T13:14:05.724885500');
