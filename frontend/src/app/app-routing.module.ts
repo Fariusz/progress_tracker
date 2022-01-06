5928555ba0e5289f461b0781a08c18eaeb1b94d0
Radosław Loth <r.loth@icloud.com>
2022-01-06 19:33:37 +0100

    Added authGuard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import {HomeComponent} from "./components/home/home.component";
 import {MainComponent} from "./components/main/main.component";
 import {ActivityComponent} from "./components/activity/activity.component";
 import {LoginComponent} from "./components/login/login.component";
+import {AuthGuardService} from "./components/auth/auth-guard.service";
 
 const routes: Routes = [
   {path: '', component: MainComponent},
-  {path: 'activity', component: ActivityComponent},
-  {path: 'home', component: HomeComponent},
+  {path: 'activity', component: ActivityComponent, canActivate: [AuthGuardService]},
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuardService]},
   {path: 'login', component: LoginComponent},
 ];
 
