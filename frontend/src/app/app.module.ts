3f7634e4d3bd1ea59f4314d41b519c6828440019
Radosław Loth <r.loth@icloud.com>
2022-01-07 16:51:25 +0100

    Added font awesome

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MainComponent } from './components/main/main.component';
 import { LoginComponent } from './components/login/login.component';
 import {ActivityService} from "./components/activity/activity.service";
 import {AuthInterceptorService} from "./components/auth/auth-interceptor.service";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import {AuthInterceptorService} from "./components/auth/auth-interceptor.service
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FontAwesomeModule
   ],
   providers: [
     ActivityService,
--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import {AuthService} from "../auth/auth.service";
 
 @Component({
