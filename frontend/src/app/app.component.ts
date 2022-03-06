6732a0941a277128473b1f60744b3dfec087f59b
Radosław Loth <r.loth@icloud.com>
2022-03-06 19:18:38 +0100

    get started with internationalization

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "./components/auth/auth.service";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,15 @@ import {AuthService} from "./components/auth/auth.service";
 export class AppComponent implements OnInit {
   title = 'Progress tracker';
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private translate: TranslateService) {
+    translate.setDefaultLang('en');
   }
 
   ngOnInit() {
     this.authService.autoLogin();
+  }
+
+  useLanguage(language: string): void {
+    this.translate.use(language);
   }
 }
