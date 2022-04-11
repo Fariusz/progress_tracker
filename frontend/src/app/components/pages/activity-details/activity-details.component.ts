fb8af1e64ad96482c0fa4d19a21c7ca5d8ef464e
Radosław Loth <r.loth@icloud.com>
2022-04-11 21:04:03 +0200

    update

--- frontend/src/app/components/pages/activity-details/activity-details.component.ts
+++ frontend/src/app/components/pages/activity-details/activity-details.component.ts
@@ -51,11 +51,6 @@ export class ActivityDetailsComponent implements OnInit {
     this.toastr.success(message, title);
   }
 
-  sort(type: string) {
-    console.log("sortowanie");
-    this.content.sort((a,b) =>(a.content, b.content) ? 1 : -1);
-  }
-
   //Modal
   modalConfig: ModalConfig = {
     modalTitle: "undefined",
